ee8e149eec081c5a17a7a8a07f76bbd54740061a
Nicholas Nassar <niccholaspage@gmail.com>
2021-04-11 00:21:11 -0400

    Revert "Temporarily remove publishing logic"
    This reverts commit 9a223df9f9b10d3782059cab01c6f429663bb153.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -13,3 +13,26 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
     implementation(kotlin("stdlib"))
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/niccholaspage/dsl-builder")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = "dsl-builder-api"
+            version = project.version as String
+
+            from(components["java"])
+        }
+    }
+}
