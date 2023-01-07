1c5708b55acbd21611838adaefedfa2acb3b81e7
Nicholas Nassar <niccholaspage@gmail.com>
2023-01-07 16:43:41 -0500

    Minor documentation for GenerateBuilder and NullValue

--- api/src/main/kotlin/com/nicholasnassar/dslbuilder/api/annotation/GenerateBuilder.kt
+++ api/src/main/kotlin/com/nicholasnassar/dslbuilder/api/annotation/GenerateBuilder.kt
@@ -1,5 +1,9 @@
 package com.nicholasnassar.dslbuilder.api.annotation
 
+/**
+ * This annotation can be placed on any class you would
+ * like to generate a builder for.
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class GenerateBuilder
--- api/src/main/kotlin/com/nicholasnassar/dslbuilder/api/annotation/NullValue.kt
+++ api/src/main/kotlin/com/nicholasnassar/dslbuilder/api/annotation/NullValue.kt
@@ -1,5 +1,10 @@
 package com.nicholasnassar.dslbuilder.api.annotation
 
+/**
+ * An annotation that can be used to associate a default value
+ * with a property so that if it is not specified, the default
+ * value is used.
+ */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.SOURCE)
 annotation class NullValue(val nullValue: String)
