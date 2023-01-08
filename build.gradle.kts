cfb27d691cb4929dcc83c6e9bd40b7c2e97d452b
Nicholas Nassar <niccholaspage@gmail.com>
2023-01-07 19:41:26 -0500

    Update to version 0.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "com.nicholasnassar.dslbuilder"
-    version = "0.0.1"
+    version = "0.0.2-SNAPSHOT"
 
     pluginManager.withPlugin("java") {
         configure<JavaPluginExtension> {
